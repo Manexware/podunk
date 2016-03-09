c8b762e56774861e6070e52c522cd3db22a759e6
eudoro <manuel.vega@manexware.com>
2016-03-09 08:53:51 -0500

    Commit: reingenieria package

---
+++ manexreport/prefab/styles.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------------
+#   file:       podunk/prefab/styles.py
+#   author:     Jim Storch
+#------------------------------------------------------------------------------
+
+from manexreport.prefab import alignment
+from manexreport.widget.style import Style
+
+#-------------------------------------------------------------------------Money
+
+from  manexreport.prefab.fonts import DEJAVU_SANS_MONO
+
+MONEY = Style()
+MONEY.font = DEJAVU_SANS_MONO
+MONEY.horizontal_alignment = alignment.RIGHT
+
+#-------------------------------------------------------------------------Header
+
+HEADER_STYLE = Style()
+HEADER_STYLE.horizontal_alignment = alignment.RIGHT
+
+
